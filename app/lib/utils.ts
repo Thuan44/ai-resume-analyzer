77479774d800064bed4d0892dd9ee0be1e6abc0e
Cong-Thuan DO <ctdo@gingerminds.fr>
2025-07-31 15:34:26 +0200

    feat(resume): create feedback components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,6 @@
+import clsx, { type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function formatSize(bytes: number): string {
     if (bytes < 1024) return `${bytes} B`
     if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`
@@ -7,3 +10,7 @@ export function formatSize(bytes: number): string {
 }
 
 export const generateUUID = () => crypto.randomUUID()
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
